1c308ab44ecea0399989e3cdfd7f3467f41350c6
Sean Altemose <sean@seanaltemose.com>
2018-06-29 01:57:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    if !(@current_user = User.find_by(session[:user_id]))
+    @current_user = User.find_by(session[:user_id])
       redirect :'/error'
     end
 
