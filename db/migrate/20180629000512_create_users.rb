257df0a216401aa236dbbf29437c33d80a5208e6
Sean Altemose <sean@seanaltemose.com>
2018-06-29 00:07:47 +0000

    automatically backed up by learn

--- db/migrate/20180629000512_create_users.rb
+++ db/migrate/20180629000512_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table Users do |t|
-      t.string :username 
-      t.string :password 
+      t.string :username
+      t.string :password
       t.float :balance
   end
 end
