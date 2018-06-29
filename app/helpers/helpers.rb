a9f89fc413b51f5e8e58a7fb0271a9f70ba62e55
Sean Altemose <sean@seanaltemose.com>
2018-06-29 02:06:08 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,8 @@
 class Helpers
 
   def self.current_user(session_hash)
-      current_user = User.find_by_id(session_hash)
-      current_user
+      @current_user = User.find_by_id(session_hash)
+      @current_user
   end
 
   def self.is_logged_in?(session_hash)
